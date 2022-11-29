726a33c4db4547595935aa439fb7f8a6d5ed72c1
mukundvarma <mmv1301@gmail.com>
2022-11-28 23:30:57 -0500

    sign up app init

--- app/main.py
+++ app/main.py
@@ -2,20 +2,44 @@
 import streamlit as st
 import pandas as pd
 
+
+@st.cache(allow_output_mutation=True)
+def persistdata():
+    defs = {
+        "Dragos": "V",
+        "Kat": "T",
+        "Mukund": "V",
+        "Viral": "M"
+    }
+    return defs
+
+
 def main():
     """Main function of the App"""
-    st.sidebar.title("Navigation")
-    selection = st.sidebar.radio("Go to", ["Page 1", "Page 2"])
+    signups = persistdata()
 
-    st.sidebar.title("About")
-    st.sidebar.info("""This app is a project template for streamplit apps.""")
+    st.title("Ski 2023")
+    with st.form("signups"):
+        fn = st.text_input("First name", value='Rando')
+        li = st.text_input("Last initial", value='X')
+        stoked = st.checkbox("Are you stoked?")
+   
+        submitted = st.form_submit_button("Submit")
 
-    st.title("Template streamlit app")
-    st.write("Some **markdown** placeholder *text*.")
+        if submitted:
+            if not stoked:
+                st.error("ARE YOU STOKED?")
+            signups[fn] = li
 
-    prices = pd.read_csv("data/prices.csv")
-    st.write("Some price data:")
-    st.write(prices)
+        st.snow()
+        
+    spots_remaining = 22 - len(signups)
+    st.write(f"Spots remaining: {spots_remaining}")
+
+    st.write("Current signups")
+    df = pd.Series(signups).to_frame().reset_index()
+    df.columns = ['First Name', 'Last Initial']
+    st.write(df)
 
 if __name__ == "__main__":
     main()
